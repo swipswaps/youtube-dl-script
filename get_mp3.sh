bb61cd8f1f15d444741414adc8b8c566d2f6145d
Catalin Ilinca <c@talin.ro>
2016-09-26 12:41:58 +0300

    mp3 with embeded thumbnail

--- get_mp3.sh
+++ get_mp3.sh
@@ -4,7 +4,7 @@ input="url_list"
 _downloader="youtube-dl"
 _converter="ffmpeg"
 
-$_downloader -c --extract-audio --audio-format mp3 --audio-quality 160K --title --batch-file=$input --restrict-filenames
+$_downloader -c --embed-thumbnail --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 160K --title --batch-file=$input --restrict-filenames
 
 mkdir -p ./mp3
 mv *.mp3 ./mp3
